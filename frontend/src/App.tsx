432d1c0439ce0c0e0d82a1fbce11b13025c94550
wesleywil <wesleywilsonti@gmail.com>
2023-03-07 14:27:53 -0300

    updated App.tsx by adding the navbar component and the new created pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//Pages
+import Homepage from "./pages/homepage/homepage";
+import Catalog from "./pages/catalog/catalog";
 import SignIn from "./pages/signIn/signIn";
+
+//Components
+import Navbar from "./components/navbar/navbar.component";
 
 //Styles
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<SignIn />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-full h-screen w-screen">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/login" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
