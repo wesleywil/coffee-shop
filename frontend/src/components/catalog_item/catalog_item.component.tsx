13b3ba4817330eb7be10360893b5a6935cf0a965
wesleywil <wesleywilsonti@gmail.com>
2023-03-09 14:26:38 -0300

    updated catalog_item component by changing the styles

--- frontend/src/components/catalog_item/catalog_item.component.tsx
+++ frontend/src/components/catalog_item/catalog_item.component.tsx
@@ -11,23 +11,24 @@ const CatalogItem = ({ title, price, description }: CatalogItemType) => {
 
   return (
     <div
-      className="relative  w-56 p-2 rounded"
+      className="relative w-72 p-2 rounded "
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="p-2 bg-black/20 backdrop-blur-sm flex flex-row gap-2 z-20">
+      <div className="p-2 text-xl font-bold bg-black/20 backdrop-blur-sm flex flex-row gap-2 z-20 rounded-xl overflow-x-auto">
         <h1>{title}</h1> - <h1>$ {price}</h1>
       </div>
       <div
-        className="bg-red-600 absolute top-full left-0  w-full px-2 rounded z-10"
+        className="bg-black text-white text-center absolute top-full left-0  w-full px-2 rounded z-10"
         style={{
-          maxHeight: isHovered ? "250px" : "0",
+          maxHeight: isHovered ? "280px" : "0",
           transition: "max-height 0.5s ease-in-out",
           overflow: "hidden",
         }}
       >
-        <p>{description}</p>
-        <button className="w-1/3 bg-black text-white mb-2 px-2 py-1">
+        <h2 className="text-2xl font-bold">Description</h2>
+        <p className="p-2">{description}</p>
+        <button className="w-full bg-white hover:bg-gray-200 text-black font-bold mb-2 px-2 py-1 rounded-xl">
           Order
         </button>
       </div>
