0648b4d441cd5b60859ed7b12ae8fa23ac8f68fb
wesleywil <wesleywilsonti@gmail.com>
2023-03-10 16:46:15 -0300

    updated catalog page, catalog_categories component by adding the switch list of products by categories

--- frontend/src/components/catalog_categories/catalog_categories.component.tsx
+++ frontend/src/components/catalog_categories/catalog_categories.component.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "../../redux/store";
+import { switch_categories } from "../../redux/utils/utils";
+
 const CatalogCategories = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [selectedOption, setSelectedOption] = useState("coffee");
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    setSelectedOption(selectedValue);
+    dispatch(switch_categories(selectedValue));
+  };
   return (
     <div className="self-center border-y border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -6,35 +19,39 @@ const CatalogCategories = () => {
           {/* <!-- Tabs --> */}
           <div className="hidden md:block">
             <div className="flex space-x-4">
-              <a
-                href="#"
+              <button
+                onClick={() => dispatch(switch_categories("coffee"))}
                 className="text-gray-500 hover:text-gray-700 font-medium text-lg  px-4 rounded-lg"
               >
                 Coffee
-              </a>
+              </button>
               <span className="text-gray-500 font-medium text-lg ">/</span>
-              <a
-                href="#"
+              <button
+                onClick={() => dispatch(switch_categories("tea"))}
                 className="text-gray-500 hover:text-gray-700 font-medium text-lg  px-4 rounded-lg"
               >
                 Tea
-              </a>
+              </button>
               <span className="text-gray-500 font-medium text-lg ">/</span>
-              <a
-                href="#"
+              <button
+                onClick={() => dispatch(switch_categories("treatsAndSweets"))}
                 className="text-gray-500 hover:text-gray-700 font-medium text-lg  px-4 rounded-lg"
               >
                 Treats and Sweets
-              </a>
+              </button>
             </div>
           </div>
 
           {/* <!-- Dropdown (mobile) --> */}
           <div className="md:hidden">
-            <select className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-              <option>Coffee</option>
-              <option>Tea</option>
-              <option>Treats and Sweets</option>
+            <select
+              className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              defaultValue={selectedOption}
+              onChange={handleChange}
+            >
+              <option value="coffee">Coffee</option>
+              <option value="tea">Tea</option>
+              <option value="treatsAndSweets">Treats and Sweets</option>
             </select>
           </div>
         </div>
--- frontend/src/pages/catalog/catalog.tsx
+++ frontend/src/pages/catalog/catalog.tsx
@@ -1,40 +1,62 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "../../redux/store";
+
 import CatalogCategories from "../../components/catalog_categories/catalog_categories.component";
 import CatalogItem from "../../components/catalog_item/catalog_item.component";
 
 const Catalog = () => {
+  const categories_item = useSelector(
+    (state: RootState) => state.utils.categories_tab
+  );
   return (
     <div>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center pb-4">
         <div className="mt-36 self-center flex justify-center gap-2 p-4">
-          <div className="mr-16 hidden md:block">
+          <div className="flex-none mr-16 hidden md:block border">
             <img src="https://dummyimage.com/600x600" alt="Cup of coffee" />
           </div>
-          <div className="flex flex-col gap-1 w-full md:w-1/3">
+          <div className="flex-none flex flex-col gap-1 w-11/12 md:w-[40rem] border">
             <h3 className="text-2xl font-bold text-center mb-2">Catalog</h3>
 
             <CatalogCategories />
 
-            <div className="flex flex-wrap justify-center gap-2">
-              <CatalogItem
-                title="Coffee Name 1"
-                price={12.0}
-                description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
-              />
-              <CatalogItem
-                title="Coffee Name 2"
-                price={14.0}
-                description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
-              />
-              <CatalogItem
-                title="Coffee Name 3"
-                price={16.99}
-                description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
-              />
-              <CatalogItem
-                title="Coffee Name 4"
-                price={8.5}
-                description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
-              />
+            <div className="w-full flex flex-wrap justify-center gap-2">
+              {categories_item === "coffee" ? (
+                <>
+                  <CatalogItem
+                    title="Coffee Name 1"
+                    price={12.0}
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
+                  />
+                  <CatalogItem
+                    title="Coffee Name 2"
+                    price={14.0}
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
+                  />
+                  <CatalogItem
+                    title="Coffee Name 3"
+                    price={16.99}
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
+                  />
+                  <CatalogItem
+                    title="Coffee Name 4"
+                    price={8.5}
+                    description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
+                  />
+                </>
+              ) : categories_item === "tea" ? (
+                <CatalogItem
+                  title="Tea Name 1"
+                  price={5.5}
+                  description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
+                />
+              ) : (
+                <CatalogItem
+                  title="Sweats and Treats Name 1"
+                  price={2.5}
+                  description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nisi blanditiis labore totam cupiditate quisquam quae nemo dolores quas sed iste ut, tempora in, nulla explicabo? Quo esse autem debitis!"
+                />
+              )}
             </div>
           </div>
         </div>
