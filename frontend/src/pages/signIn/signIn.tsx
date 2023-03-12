d94dd7ffde77d48e29c246ec9510a22ffb9cd8aa
wesleywil <wesleywilsonti@gmail.com>
2023-03-12 13:55:17 -0300

    updated signIn page by adding the loginUser function from the accountsSlice

--- frontend/src/pages/signIn/signIn.tsx
+++ frontend/src/pages/signIn/signIn.tsx
@@ -1,28 +1,67 @@
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store";
+import { Link } from "react-router-dom";
+import { loginUser } from "../../redux/accounts/accounts";
+
+interface CustomElements extends HTMLFormControlsCollection {
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+}
+
+interface CustomForm extends HTMLFormElement {
+  readonly elements: CustomElements;
+}
+
 const SignIn = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const handleLogin = (e: React.FormEvent<CustomForm>) => {
+    e.preventDefault();
+    const target = e.currentTarget.elements;
+
+    const data = {
+      email: target.email.value,
+      password: target.password.value,
+    };
+    dispatch(loginUser(data));
+  };
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-4xl font-bold mt-48">Login Page</h1>
-      <form className="self-center mt-2  w-1/4 h-80 text-3xl text-white flex flex-col items-center bg-black p-2 rounded-xl">
+      <div className="mt-48">
+        <h1 className="text-4xl font-bold ">Login Page</h1>
+      </div>
+
+      <form
+        onSubmit={handleLogin}
+        className="self-center mt-2  w-1/4 h-80 text-3xl text-white flex flex-col items-center bg-black p-2 rounded-xl"
+      >
         <span>Email</span>
         <input
           type="email"
+          name="email"
           placeholder="Your e-mail goes here!"
           className="w-full text-black text-center"
         />
         <span>Password</span>
         <input
           type="password"
+          name="password"
           placeholder="Your password goes here!"
           className="w-full text-black text-center"
         />
-        <span className="mt-2 text-base text-left hover:text-gray-400 w-full">
+        <Link
+          to="/register"
+          className="mt-2 text-base text-left hover:text-gray-400 w-full"
+        >
           Don't have an account? Click here!
-        </span>
+        </Link>
         <div className="mt-8 flex gap-4 justify-center font-bold text-2xl">
           <button className="bg-white hover:bg-gray-300 text-black  px-2 py-1 rounded-xl">
             Login
           </button>
-          <button className="bg-white hover:bg-gray-300 text-black px-2 py-1 rounded-xl">
+          <button
+            type="button"
+            className="bg-white hover:bg-gray-300 text-black px-2 py-1 rounded-xl"
+          >
             Cancel
           </button>
         </div>
