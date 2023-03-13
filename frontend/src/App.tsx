8420f632e56ded5f916da55b9f19e1965ac261c5
wesleywil <wesleywilsonti@gmail.com>
2023-03-13 18:38:57 -0300

    added a basic profile page to include the new user info that is being loaded from the API call

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "./redux/store";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Redux
+import { getUserInfo } from "./redux/accounts/accounts";
 
 //Pages
 import Homepage from "./pages/homepage/homepage";
 import Catalog from "./pages/catalog/catalog";
 import SignIn from "./pages/signIn/signIn";
 import Register from "./pages/register/register";
+import Profile from "./client/profile/profile";
 
 //Components
 import Navbar from "./components/navbar/navbar.component";
@@ -13,6 +20,16 @@ import Navbar from "./components/navbar/navbar.component";
 import "./App.css";
 
 function App() {
+  const status_user = useSelector((state: RootState) => state.accounts.status);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    console.log("Getting User Info");
+    if (status_user === "idle") {
+      dispatch(getUserInfo());
+    }
+  }, []);
+
   return (
     <div className="min-h-full h-screen w-screen">
       <Navbar />
@@ -22,6 +39,8 @@ function App() {
           <Route path="/login" element={<SignIn />} />
           <Route path="/register" element={<Register />} />
           <Route path="/catalog" element={<Catalog />} />
+          {/* Client Side Pages */}
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/client/profile/profile.tsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "../../redux/store";
+
+const Profile = () => {
+  const user = useSelector((state: RootState) => state.accounts.user);
+  const status = useSelector((state: RootState) => state.accounts.status);
+  if (status === "loading") {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div className="flex flex-col items-center h-2/3 border">
+      <div className="mt-24 text-center">
+        <h1 className="text-4xl font-bold">Welcome {user.name || "No data"}</h1>
+        <h2 className="text-2xl">{user.email || "No data"}</h2>
+        <h3>Since - {user.created_at?.slice(0, 10) || "No data"}</h3>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Profile;
