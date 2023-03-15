923e0e463e1864eb3bd243898616f8b037c6e3d0
wesleywil <wesleywilsonti@gmail.com>
2023-03-15 13:36:00 -0300

    added new Reservation_table model and migrations

---
+++ php-backend/database/migrations/2023_03_15_151439_create_reserve_tables_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reserve_tables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')->constrained()->onDelete('cascade');
+            $table->integer('seats');
+            $table->enum('status', ['occupied', 'available'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reserve_tables');
+    }
+};
