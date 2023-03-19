dd4e1fd1c5841e69ee982a2d4411d1453f418a95
wesleywil <wesleywilsonti@gmail.com>
2023-03-19 12:48:06 -0300

    updated utilsSlice by including a reservebox hidden reducer and reserve box state reducer

--- frontend/src/redux/utils/utils.tsx
+++ frontend/src/redux/utils/utils.tsx
@@ -1,11 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface SelectTable {
+  id: number;
+  seats: number;
+}
+
 export interface UtilsState {
   categories_tab: string;
+  select_table_hidden: boolean;
+  select_table: SelectTable;
 }
 
 const initialState: UtilsState = {
   categories_tab: "coffee" || "tea" || "treatsAndSweets",
+  select_table_hidden: true,
+  select_table: { id: 0, seats: 0 },
 };
 
 export const utilsSlice = createSlice({
@@ -15,9 +24,23 @@ export const utilsSlice = createSlice({
     switch_categories: (state, { payload }) => {
       state.categories_tab = payload;
     },
+    set_select_table_hidden: (state, { payload }) => {
+      state.select_table_hidden = payload;
+    },
+    switch_hidden_select_table: (state) => {
+      state.select_table_hidden = !state.select_table_hidden;
+    },
+    select_table: (state, { payload }) => {
+      state.select_table = payload;
+    },
   },
 });
 
-export const { switch_categories } = utilsSlice.actions;
+export const {
+  switch_categories,
+  set_select_table_hidden,
+  switch_hidden_select_table,
+  select_table,
+} = utilsSlice.actions;
 
 export default utilsSlice.reducer;
