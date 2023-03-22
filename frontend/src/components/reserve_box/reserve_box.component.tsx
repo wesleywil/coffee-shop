4d90a79abd307fd71d137d154a22d1f5d857e83e
wesleywil <wesleywilsonti@gmail.com>
2023-03-21 22:34:10 -0300

    updated ReserveBox component by improving the style

--- frontend/src/components/reserve_box/reserve_box.component.tsx
+++ frontend/src/components/reserve_box/reserve_box.component.tsx
@@ -2,7 +2,6 @@ import { useSelector, useDispatch } from "react-redux";
 import type { RootState, AppDispatch } from "../../redux/store";
 import { set_select_table_hidden } from "../../redux/utils/utils";
 import { createReservation } from "../../redux/reservations/reservations";
-import React from "react";
 
 const ReserveBox = () => {
   const today = new Date().toISOString().slice(0, 10);
@@ -10,8 +9,7 @@ const ReserveBox = () => {
   const reserve = useSelector((state: RootState) => state.utils.select_table);
   const dispatch = useDispatch<AppDispatch>();
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     const data = {
       reserve_date: today,
       table_id: reserve.id,
@@ -20,32 +18,24 @@ const ReserveBox = () => {
     dispatch(set_select_table_hidden(true));
   };
   return (
-    <div className="bg-[#70351B]/70 backdrop-blur-sm p-2 px-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded">
-      <div className="flex flex-col items-center text-xl text-[#F3EFE6]">
-        <h1>Reservation</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col ">
-          <h1>Selected Table - Seats {reserve.seats}</h1>
-          <span>Reserve Date</span>
-          <input
-            type="date"
-            className="text-[#F3EFE6] bg-[#D87D4A] rounded"
-            disabled
-            defaultValue={today}
-          />
-          <div className="mt-4 flex gap-4 justify-center font-bold text-[#F3EFE6]">
-            <button className="bg-[#D87D4A] hover:bg-[#F3EFE6] hover:text-[#70351B] px-2 rounded">
-              Reserve
-            </button>
-            <button
-              onClick={() => dispatch(set_select_table_hidden(true))}
-              className="bg-[#D87D4A] hover:bg-[#F3EFE6] hover:text-[#70351B] px-2 rounded"
-              type="button"
-            >
-              Close
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className="mt-2 bg-[#F3EFE6] text-[#70351B] text-xl font-bold px-2 py-1 flex flex-col shadow-lg items-center rounded">
+      <h1>Confirm reserve to {today}?</h1>
+      <h1>Table - {reserve.seats}</h1>
+      <h1>
+        <button
+          onClick={handleSubmit}
+          className="bg-[#70351B] hover:bg-[#70351B]/40 text-[#F3EFE6] px-2"
+        >
+          YES
+        </button>{" "}
+        |{" "}
+        <button
+          onClick={() => dispatch(set_select_table_hidden(true))}
+          className="bg-[#70351B] hover:bg-[#70351B]/40 text-[#F3EFE6] px-2"
+        >
+          NO
+        </button>{" "}
+      </h1>
     </div>
   );
 };
