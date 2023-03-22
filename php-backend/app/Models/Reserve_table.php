4c34b87f49734a60468d631cf32a126d02b93f2d
wesleywil <wesleywilsonti@gmail.com>
2023-03-21 22:45:45 -0300

    updated Reserve_table model by adding a logic to verify if a reserve already passed the reserve_date the table will be set to available and the reserve will be set to closed

--- php-backend/app/Models/Reserve_table.php
+++ php-backend/app/Models/Reserve_table.php
@@ -5,9 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Reservation;
+
 class Reserve_table extends Model
 {
     protected $fillable = [
-        'seats','status'
+        'seats',
+        'status'
     ];
+
+    protected static function booted()
+    {
+        static::retrieved(function ($table) {
+            if ($table->reservation && $table->reservation->reserve_date < date('Y-m-d')) {
+                $table->update(['status' => 'available']);
+                $table->reservation->update(['status' => 'closed']);
+            }
+        });
+    }
+
+    public function reservation()
+    {
+        return $this->hasOne(Reservation::class, 'reserve_tables_id');
+    }
 }
