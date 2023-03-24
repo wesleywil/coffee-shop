6d4869fca1ceb2546d452a2cf2f1af6e69978714
wesleywil <wesleywilsonti@gmail.com>
2023-03-24 13:16:39 -0300

    updated order page and OrderItem component by including the products, quantity and total from the Cart component

--- frontend/src/components/order_item/order_item.component.tsx
+++ frontend/src/components/order_item/order_item.component.tsx
@@ -1,29 +1,44 @@
+import { useState } from "react";
 import { FaPlus, FaMinus, FaTrashAlt } from "react-icons/fa";
 
-const OrderItem = () => {
+type OrderItemType = {
+  img: string;
+  product_id: number;
+  product_name: string;
+  quantity: number;
+  price: number;
+};
+
+const OrderItem = ({ ...item }: OrderItemType) => {
   return (
     <div className="w-full flex justify-between items-center px-2 mb-2 text-[#F3EFE6]">
       {/* Image */}
       <img
-        src="https://dummyimage.com/200x200"
+        src={item.img}
         alt=""
         className="self-center w-16 xl:w-24 rounded-xl"
       />
       {/* Product Info */}
       <div className="flex flex-col items-center text-xl">
-        <h2 className="self-center">Product name</h2>
+        <h2 className="self-center">
+          {item.product_name.length > 13
+            ? item.product_name.substring(0, 13) + "..."
+            : item.product_name}
+        </h2>
         <div className="self-center w-20 text-base flex justify-between">
           <button className="p-1 bg-[#D87D4A] hover:bg-[#F3EFE6] text-[#F3EFE6] hover:text-[#70351B] font-bold rounded">
             <FaMinus />
           </button>
-          <h2>1</h2>
+          <h2>{item.quantity}</h2>
           <button className="p-1 bg-[#D87D4A] hover:bg-[#F3EFE6] text-[#F3EFE6] hover:text-[#70351B] font-bold rounded">
             <FaPlus />
           </button>
         </div>
       </div>
       {/* Price */}
-      <h1 className="h-12 flex items-center self-center  text-xl">$ 00.00</h1>
+      <h1 className="w-18 h-12 pl-2 flex items-center justify-start self-center text-xl">
+        $ {item.price.toFixed(2)}
+      </h1>
       {/* Remove Product */}
       <button className="h-12 w-12 flex justify-center items-center p-1 bg-[#D87D4A] hover:bg-[#F3EFE6] text-[#F3EFE6] hover:text-[#70351B] text-2xl font-bold rounded">
         <FaTrashAlt />
--- frontend/src/pages/order/order.tsx
+++ frontend/src/pages/order/order.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../redux/store";
 import OrderItem from "../../components/order_item/order_item.component";
 
 const Order = () => {
+  const order_items = useSelector(
+    (state: RootState) => state.cart.cart_products
+  );
+  const total = useSelector((state: RootState) => state.cart.total);
+
+  useEffect(() => {
+    console.log("ORDER ITEMS => ", order_items);
+  }, [order_items]);
+
   return (
     <div className="flex flex-col items-center p-1 md:p-4">
       <h1 className=" xl:mt-12 md:mt-20 text-4xl text-[#F3EFE6] font-bold text-center mb-2">
@@ -11,15 +23,31 @@ const Order = () => {
           Products in Order
         </h2>
         <div className="py-2 border-y border-[#D87D4A] ">
+          {order_items.length ? (
+            order_items.map((item) => (
+              <OrderItem
+                key={item.product.id}
+                img={item.product.image}
+                product_name={item.product.title}
+                product_id={item.product.id}
+                quantity={item.quantity}
+                price={item.product.price}
+              />
+            ))
+          ) : (
+            <h1 className="text-[#F3EFE6] font-bold text-xl text-center">
+              NO ITEMS BEING ORDERED
+            </h1>
+          )}
+          {/* <OrderItem />
           <OrderItem />
           <OrderItem />
           <OrderItem />
-          <OrderItem />
-          <OrderItem />
+          <OrderItem /> */}
         </div>
         <div className="text-center text-4xl text-[#F3EFE6] font-bold">
           <h1>Total</h1>
-          <h2 className="my-2 text-xl">$00.00</h2>
+          <h2 className="my-2 text-xl">${total}</h2>
         </div>
         <div className="border-y border-[#D87D4A] p-2 mb-2">
           <button className="w-full p-1 bg-[#D87D4A] hover:bg-[#F3EFE6] text-[#F3EFE6] hover:text-[#70351B] text-2xl font-bold rounded">
