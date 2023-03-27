d4e4d4e3e4da25fabb0dc2b146a8096f414f0560
wesleywil <wesleywilsonti@gmail.com>
2023-03-27 12:05:10 -0300

    updated homepage by making more responsive

--- frontend/src/pages/homepage/homepage.tsx
+++ frontend/src/pages/homepage/homepage.tsx
@@ -1,24 +1,28 @@
 const Homepage = () => {
   return (
     <div className="flex flex-row xl:flex-col justify-center items-center">
-      <div className="mt-36 self-center flex justify-center gap-2 p-4">
-        <div className="mr-16">
-          <img src="https://dummyimage.com/600x600" alt="Cup of coffee" />
+      <div className="mt-12 md:mt-36 self-center flex flex-col-reverse xl:flex-row justify-center gap-2 p-4">
+        <div className="xl:mr-16 mx-auto">
+          <img
+            src="https://dummyimage.com/600x600"
+            alt="Cup of coffee"
+            className="rounded-xl"
+          />
         </div>
-        <div className=" self-center flex flex-col gap-1 w-1/3">
-          <h3 className="text-xl text-[#F3EFE6]">
+        <div className=" self-center flex flex-col gap-1  text-center md:w-2/3 xl:w-1/3 mx-auto">
+          <h3 className="md:text-xl text-[#F3EFE6]">
             Experience Exceptional Quality
           </h3>
-          <h1 className="my-4 font-bold text-8xl text-[#F3EFE6]">
+          <h1 className="my-4 font-bold text-5xl md:text-8xl text-[#F3EFE6]">
             Discover Your Perfect Cup
           </h1>
-          <p className="text-[#F3EFE6]">
+          <p className="text-[#F3EFE6] md:text-2xl xl:text-base hidden md:block ">
             Our mission is to bring you the best coffee experience through
             sustainably sourced, hand-picked beans. We are passionate about
             great coffee and believe that every cup should be a journey. Join us
             in our commitment to excellence.
           </p>
-          <button className="w-fit mt-12 px-2 py-1 bg-[#D87D4A] font-bold text-xl text-[#F3EFE6] float-left  rounded-xl">
+          <button className="w-fit mx-auto md:mt-12 px-2 py-1 bg-[#D87D4A] font-bold text-xl text-[#F3EFE6] float-left  rounded-xl">
             Open the Catalog
           </button>
         </div>
