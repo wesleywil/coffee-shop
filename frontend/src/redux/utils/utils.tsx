3d23de19e03b52c78e178d6954cfa60a9c363741
wesleywil <wesleywilsonti@gmail.com>
2023-03-31 13:18:20 -0300

    updated utilsSlice by adding a new state and reducer to control if the order_details component will be hidden or not

--- frontend/src/redux/utils/utils.tsx
+++ frontend/src/redux/utils/utils.tsx
@@ -9,12 +9,14 @@ export interface UtilsState {
   categories_tab: string;
   select_table_hidden: boolean;
   select_table: SelectTable;
+  order_details_hidden: boolean;
 }
 
 const initialState: UtilsState = {
   categories_tab: "coffee" || "tea" || "treatsAndSweets",
   select_table_hidden: true,
   select_table: { id: 0, seats: 0 },
+  order_details_hidden: true,
 };
 
 export const utilsSlice = createSlice({
@@ -30,6 +32,9 @@ export const utilsSlice = createSlice({
     switch_hidden_select_table: (state) => {
       state.select_table_hidden = !state.select_table_hidden;
     },
+    switch_order_hidden: (state) => {
+      state.order_details_hidden = !state.order_details_hidden;
+    },
     select_table: (state, { payload }) => {
       state.select_table = payload;
     },
@@ -40,6 +45,7 @@ export const {
   switch_categories,
   set_select_table_hidden,
   switch_hidden_select_table,
+  switch_order_hidden,
   select_table,
 } = utilsSlice.actions;
 
