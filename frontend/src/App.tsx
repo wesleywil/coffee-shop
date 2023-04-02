7cd82912539325297130abc49bf54cef18dba0ff
wesleywil <wesleywilsonti@gmail.com>
2023-04-02 14:05:15 -0300

    updated App.tsx by adding the new payment page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "./redux/store";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 // Redux
 import { getUserInfo } from "./redux/accounts/accounts";
@@ -16,6 +18,7 @@ import Profile from "./client/pages/profile/profile";
 import Tables from "./pages/tables/tables";
 import Order from "./pages/order/order";
 import MyOrders from "./client/pages/my_orders/my_orders";
+import Payment from "./pages/payment/payment";
 
 //Components
 import Navbar from "./components/navbar/navbar.component";
@@ -25,6 +28,9 @@ import WithReservation from "./components/withReservation/withReservation.compon
 import "./App.css";
 
 function App() {
+  const stripePromise = loadStripe(
+    "pk_test_51KuIEXF2kW82FaU4ECtARLaKr9qhfEnJbtlfgE1Jod1K41p9lrb3mMqPTjI3Nb4et1h8SDL8vhiF8bIeCsHFFG3d00jYih1pVE"
+  );
   const status_user = useSelector((state: RootState) => state.accounts.status);
   const dispatch = useDispatch<AppDispatch>();
 
@@ -40,6 +46,19 @@ function App() {
     }
   }, []);
 
+  const appearance = {
+    variables: {
+      colorPrimary: "#0570de",
+      colorBackground: "#70351B",
+      colorText: "#F3EFE6",
+      colorDanger: "#D87D4A",
+      fontFamily: "Ideal Sans, system-ui, sans-serif",
+      spacingUnit: "2px",
+      borderRadius: "4px",
+      // See all possible variables below
+    },
+  };
+
   return (
     <div className="bg-[#4F2B1D] h-screen min-h-full w-screen overflow-y-auto">
       <BrowserRouter>
@@ -54,6 +73,22 @@ function App() {
           {/* Client Side Pages */}
           <Route path="/profile" element={<Profile />} />
           <Route path="/myorders" element={WithReservation(MyOrders)} />
+          {/* Test Page */}
+          <Route
+            path="/test"
+            element={
+              <Elements
+                stripe={stripePromise}
+                options={{
+                  mode: "setup",
+                  currency: "usd",
+                  appearance: appearance,
+                }}
+              >
+                <Payment />
+              </Elements>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
